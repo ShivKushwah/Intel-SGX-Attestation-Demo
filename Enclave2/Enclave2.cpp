347163ef9f23bc8602edbe568096844fb695c8b9
Shiv Kushwah <shivendrakushwah@gmail.com>
2019-06-16 11:41:41 -0700

    adding more session request

--- Enclave2/Enclave2.cpp
+++ Enclave2/Enclave2.cpp
@@ -4,7 +4,69 @@
 #include "sgx_eid.h"
 #include "sgx_dh.h"
 
+//datatypes.h
+typedef struct _session_id_tracker_t
+{
+    uint32_t          session_id;
+}session_id_tracker_t;
+
+//error_codes.h
 typedef uint32_t ATTESTATION_STATUS;
+
+#define SUCCESS                          0x00
+#define INVALID_PARAMETER                0xE1
+#define VALID_SESSION                    0xE2
+#define INVALID_SESSION                  0xE3
+#define ATTESTATION_ERROR                0xE4
+#define ATTESTATION_SE_ERROR             0xE5
+#define IPP_ERROR                        0xE6
+#define NO_AVAILABLE_SESSION_ERROR       0xE7
+#define MALLOC_ERROR                     0xE8
+#define ERROR_TAG_MISMATCH               0xE9
+#define OUT_BUFFER_LENGTH_ERROR          0xEA
+#define INVALID_REQUEST_TYPE_ERROR       0xEB
+#define INVALID_PARAMETER_ERROR          0xEC
+#define ENCLAVE_TRUST_ERROR              0xED
+#define ENCRYPT_DECRYPT_ERROR            0xEE
+#define DUPLICATE_SESSION                0xEF
+
+//dh_session_protocol.h
+#include "sgx_ecp_types.h"
+#include "sgx_key.h"
+#include "sgx_report.h"
+#include "sgx_attributes.h"
+
+#define NONCE_SIZE         16
+#define MAC_SIZE           16
+
+#define MSG_BUF_LEN        sizeof(ec_pub_t)*2
+#define MSG_HASH_SZ        32
+
+
+//Session information structure
+typedef struct _la_dh_session_t
+{
+    uint32_t  session_id; //Identifies the current session
+    uint32_t  status; //Indicates session is in progress, active or closed
+    union
+    {
+        struct
+        {
+			sgx_dh_session_t dh_session;
+        }in_progress;
+
+        struct
+        {
+            sgx_key_128bit_t AEK; //Session Key
+            uint32_t counter; //Used to store Message Sequence Number
+        }active;
+    };
+} dh_session_t;
+
+#define MAX_SESSION_COUNT  16
+
+session_id_tracker_t *g_session_id_tracker[MAX_SESSION_COUNT];
+
 
 
 int generate_random_number2() {
@@ -12,13 +74,36 @@ int generate_random_number2() {
     return 41;
 }
 
+//Returns a new sessionID for the source destination session
+ATTESTATION_STATUS generate_session_id(uint32_t *session_id)
+{
+    ATTESTATION_STATUS status = SUCCESS;
+
+    if(!session_id)
+    {
+        return INVALID_PARAMETER_ERROR;
+    }
+    //if the session structure is untintialized, set that as the next session ID
+    for (int i = 0; i < MAX_SESSION_COUNT; i++)
+    {
+        if (g_session_id_tracker[i] == NULL)
+        {
+            *session_id = i;
+            return status;
+        }
+    }
+
+    status = NO_AVAILABLE_SESSION_ERROR;
+
+    return status;
+
+}
+
 //Handle the request from Source Enclave for a session
 ATTESTATION_STATUS session_request(sgx_enclave_id_t src_enclave_id,
                           sgx_dh_msg1_t *dh_msg1,
                           uint32_t *session_id )
 {
-    ocall_print("inside session_request");
-    /* 
     dh_session_t session_info;
     sgx_dh_session_t sgx_dh_session;
     sgx_status_t status = SGX_SUCCESS;
@@ -32,7 +117,7 @@ ATTESTATION_STATUS session_request(sgx_enclave_id_t src_enclave_id,
     if(SGX_SUCCESS != status)
     {
         return status;
-    }
+    } 
     
     //get a new SessionID
     if ((status = (sgx_status_t)generate_session_id(session_id)) != SUCCESS)
@@ -44,6 +129,10 @@ ATTESTATION_STATUS session_request(sgx_enclave_id_t src_enclave_id,
     {
         return MALLOC_ERROR;
     }
+
+    ocall_print("here!");
+
+    /*
 
     memset(g_session_id_tracker[*session_id], 0, sizeof(session_id_tracker_t));
     g_session_id_tracker[*session_id]->session_id = *session_id;
